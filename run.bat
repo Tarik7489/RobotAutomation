59d83f9a79cd39d569ca2288d94a801cf2f05dfc
Tarik7489 <tarikaziz7489@gmail.com>
2025-06-30 12:29:17 +0530

    webhook updated

--- run.bat
+++ run.bat
@@ -1,8 +1,14 @@
 @echo off
-echo Activating virtual environment...
+echo Creating virtual environment if not exists...
+python -m venv .venv
 
-:: Activate from Jenkins workspace path
-call %WORKSPACE%\.venv\Scripts\activate.bat
+echo Activating virtual environment...
+call .venv\Scripts\activate.bat
+
+echo Installing dependencies...
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install robotframework-pabot
 
 echo Running Robot Tests...
 pabot --processes 1 --outputdir results TestCases\login_test.robot
